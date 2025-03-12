a3bfef869203556a890d33fa55097043e42dcc8c
atheimuz <atheimuz@naver.com>
2025-03-12 19:20:13 +0900

    feat: 헤더 적용 및 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "@/components/common/Header";
+
 function App() {
-    return <div></div>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<div />} />
+                    <Route path="/like" element={<div />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
 }
 
 export default App;

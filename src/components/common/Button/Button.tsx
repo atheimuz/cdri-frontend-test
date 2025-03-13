ae03b6935ae034091d6d6d9924d17f0e6775dba0
atheimuz <atheimuz@naver.com>
2025-03-13 15:10:11 +0900

    feat: link와 button 구분해서 사용

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,35 @@
 import cx from "classnames";
 import styles from "./Button.module.scss";
 
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface Props {
+    as?: "button" | "a";
     theme?: "primary" | "secondary" | "default";
     size?: "s" | "m";
 }
-const Button = ({ className, theme = "default", size = "m", children, ...rest }: Props) => {
+interface ButtonProps extends Props, React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface LinkProps extends Props, React.AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+const Button = ({
+    as,
+    className,
+    theme = "default",
+    size = "m",
+    children,
+    ...rest
+}: ButtonProps | LinkProps) => {
+    const classnames = cx(className, styles.button, styles[theme], styles[size]);
+
+    if (as === "a") {
+        const { ...linkProps } = rest as LinkProps;
+        return (
+            <a className={classnames} {...linkProps}>
+                {children}
+            </a>
+        );
+    }
+
     return (
-        <button className={cx(className, styles.button, styles[theme], styles[size])} {...rest}>
+        <button className={cx(classnames)} {...(rest as ButtonProps)}>
             {children}
         </button>
     );

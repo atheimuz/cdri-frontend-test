447c4bf4362628dd6304642ff7d9f1e4c8cb2b17
atheimuz <atheimuz@naver.com>
2025-03-14 19:10:21 +0900

    feat: 검색 키워드 히스토리 한곳에서 관리, 최대 개수 지정

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,31 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import SearchIcon from "@/components/icons/search";
 import SearchBoxDetail from "@/components/SearchBoxDetail";
 import SearchBoxKeyword from "@/components/SearchBoxKeyword";
 import styles from "./SearchBox.module.scss";
 
 const SearchBox = () => {
-    const navigate = useNavigate();
-
-    const onSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key !== "Enter") return;
-        const keyword = (e.target as HTMLInputElement).value;
-        navigate(`/?keyword=${keyword}`, { replace: true });
-        e.currentTarget.blur();
-    };
-
     return (
         <div className={styles.wrapper}>
-            <div className={styles.inputArea}>
-                <SearchIcon className={styles.searchIcon} />
-                <input
-                    type="text"
-                    className={styles.input}
-                    placeholder="검색어를 입력하세요"
-                    onKeyDown={onSearch}
-                />
-                <SearchBoxKeyword />
-            </div>
+            <SearchBoxKeyword />
             <div className={styles.detail}>
                 <SearchBoxDetail />
             </div>
--- src/components/SearchBoxKeyword/SearchBoxKeyword.tsx
+++ src/components/SearchBoxKeyword/SearchBoxKeyword.tsx
@@ -1,21 +1,44 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import SearchIcon from "@/components/icons/search";
 import CloseIcon from "@/components/icons/close";
 import styles from "./SearchBoxKeyword.module.scss";
 
 const STORAGE_NAME = "cdri-search-keywords";
+const MAX_KEYWORD_COUNT = 8;
 const SearchBoxKeyword = () => {
+    const navigate = useNavigate();
     const [keywordList, setKeywordList] = useState<string[]>([]);
+
+    const onAddKeyword = (keyword: string) => {
+        const storedKeywords = localStorage.getItem(STORAGE_NAME);
+        let newKeywordList = storedKeywords ? JSON.parse(storedKeywords) : [];
+        newKeywordList.unshift(keyword);
+        if (newKeywordList.length > MAX_KEYWORD_COUNT) {
+            newKeywordList = newKeywordList.slice(0, MAX_KEYWORD_COUNT);
+        }
+        localStorage.setItem(STORAGE_NAME, JSON.stringify(newKeywordList));
+        setKeywordList(newKeywordList);
+    };
 
     const onRemoveKeyword = (index: number) => {
         const newKeywordList = [...keywordList];
         newKeywordList.splice(index, 1);
         setKeywordList(newKeywordList);
-        window.localStorage.setItem(STORAGE_NAME, JSON.stringify(newKeywordList));
+        localStorage.setItem(STORAGE_NAME, JSON.stringify(newKeywordList));
+    };
+
+    const onSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== "Enter") return;
+        const keyword = (e.target as HTMLInputElement).value;
+        if (!keyword.length) return;
+        navigate(`/?keyword=${keyword}`, { replace: true });
+        e.currentTarget.blur();
+        onAddKeyword(keyword);
     };
 
     useEffect(() => {
-        const storedKeywords = window.localStorage.getItem(STORAGE_NAME);
+        const storedKeywords = localStorage.getItem(STORAGE_NAME);
         const newKeywordList = storedKeywords ? JSON.parse(storedKeywords) : [];
         if (newKeywordList.length > 0) {
             setKeywordList(newKeywordList);
@@ -23,22 +46,31 @@ const SearchBoxKeyword = () => {
     }, []);
 
     return (
-        <ul className={styles.wrapper}>
-            {keywordList.map((item, index) => (
-                <li className={styles.keyword} key={item}>
-                    <Link to={`/keyword=${item}`} replace className={styles.link}>
-                        {item}
-                    </Link>
-                    <button
-                        type="button"
-                        className={styles.removeBtn}
-                        onClick={() => onRemoveKeyword(index)}
-                    >
-                        <CloseIcon className={styles.removeIcon} />
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <div className={styles.wrapper}>
+            <SearchIcon className={styles.searchIcon} />
+            <input
+                type="text"
+                className={styles.input}
+                placeholder="검색어를 입력하세요"
+                onKeyDown={onSearch}
+            />
+            <ul className={styles.keywordList}>
+                {keywordList.map((item, index) => (
+                    <li className={styles.keyword} key={`${item}-${index}`}>
+                        <Link to={`/keyword=${item}`} replace className={styles.link}>
+                            {item}
+                        </Link>
+                        <button
+                            type="button"
+                            className={styles.removeBtn}
+                            onClick={() => onRemoveKeyword(index)}
+                        >
+                            <CloseIcon className={styles.removeIcon} />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 

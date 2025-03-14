c90926cc83cb9b39e298e2559564a741dd3eb819
atheimuz <atheimuz@naver.com>
2025-03-14 18:48:48 +0900

    feat: 키워드 검색시 링크 이동

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,14 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import SearchIcon from "@/components/icons/search";
 import SearchBoxDetail from "@/components/SearchBoxDetail";
 import SearchBoxKeyword from "@/components/SearchBoxKeyword";
 import styles from "./SearchBox.module.scss";
 
 const SearchBox = () => {
+    const navigate = useNavigate();
+
+    const onSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== "Enter") return;
+        const keyword = (e.target as HTMLInputElement).value;
+        navigate(`/?keyword=${keyword}`, { replace: true });
+        e.currentTarget.blur();
+    };
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.inputArea}>
                 <SearchIcon className={styles.searchIcon} />
-                <input type="text" className={styles.input} placeholder="검색어를 입력하세요" />
+                <input
+                    type="text"
+                    className={styles.input}
+                    placeholder="검색어를 입력하세요"
+                    onKeyDown={onSearch}
+                />
                 <SearchBoxKeyword />
             </div>
             <div className={styles.detail}>

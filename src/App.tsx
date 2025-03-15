884b14e13094a86ff519b214200cd6d3ecf24d2d
atheimuz <atheimuz@naver.com>
2025-03-15 21:35:14 +0900

    feat: react-query 설치 및 옵션 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "@/components/common/Header";
 import SearchBookPage from "@/pages/SearchBookPage";
 import LikedBookPage from "@/pages/LikedBookPage";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 20 * 60 * 1000
+        }
+    }
+});
 function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path="/" element={<SearchBookPage />} />
-                    <Route path="/like" element={<LikedBookPage />} />
-                </Routes>
-            </main>
+            <QueryClientProvider client={queryClient}>
+                <Header />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<SearchBookPage />} />
+                        <Route path="/like" element={<LikedBookPage />} />
+                    </Routes>
+                </main>
+            </QueryClientProvider>
         </BrowserRouter>
     );
 }
